3df1e7f48bcce66cf8f75949bc5548e0269efec7
eduar <eduar@EDUARDO>
2025-06-22 06:30:03 -0400

    upload files

--- auiditing_love_through_the_law.go
+++ auiditing_love_through_the_law.go
@@ -42,10 +42,39 @@ const auiditing_love_through_the_law = `
 		</div>
 
 	</section>
-	
-	<h2>
-		La ley correctamente interpretada nos ayuda a definir el amor
-	</h2>
+
+
+	<section>
+		<h2>
+			La ley correctamente interpretada nos ayuda a definir el amor
+		</h2>
+		<div class="article_section_idea">
+			<p class="article_section_idea">
+				El amor que Dios requiere de nosotros es el cumplimiento de los mandamientos, lo que involucra vivir de acuerdo a la interpretación epistolar de toda la ley.
+			</p>
+			<div class="article_section_idea_justification_items_container">
+				<ol>
+					<li>
+						<span>Rom.7.4-12</span>
+						<span>La Ley, especialmente los diez mandamientos, está abrogada.</span>
+					</li>
+					<li>
+						<span>1.Cor.9.19-22</span>
+						<span>Gal.6.1-2</span>
+						<span>Estamos bajo la ley de Cristo.</span>
+					</li>
+					<li>
+						<span>1.Tim.1.3-11</span>
+						<span>1.Jn.3.1-6</span>
+						<span>1.Tim.5.17-18</span>
+						<span>1.Cor.8.13-15</span>
+						<span>1.Ped.1.13.17</span>
+						<span>La ley de Cristo involucra cumplir todos los mandamientos de la ley interpretándolos a través de las enseñanzas y procedimiento de las epístolas.</span>
+					</li>
+				</ol>			
+			</div>
+		</div>
+	</section>
 	
 	<h2>
 		Examinemos el amor con los 10 mandamientos
