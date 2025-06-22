8b03d2e59deab69888f24cccf8752861959e5acd
eduar <eduar@EDUARDO>
2025-06-22 05:18:06 -0400

    upload files

--- page.go
+++ page.go
@@ -5,6 +5,11 @@ const PageTop = `
 	<!DOCTYPE html>
 	<html land="en">
 		<head>
+		 <meta charset="UTF-8">
+		<meta name="description" content="t3.medium">
+		<meta name="author" content="Eduardo Osteicoechea">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="static/global_styles.css">
 `
 const PageBody = `
 		</head>
