87114e9e7655e168e0168a9957a00ffe0cf5addb
eduar <eduar@EDUARDO>
2025-06-22 06:04:46 -0400

    upload files

--- auiditing_love_through_the_law.go
+++ auiditing_love_through_the_law.go
@@ -5,19 +5,25 @@ const auiditing_love_through_the_law = `
 
 <article class="article">
 
-	<h1 class="article_main_heading">
+	<h1>
 		Auditando el amor a través de la ley
 	</h1>
-	
-	<h2 class="article_heading">
+
+	<section>
+		<h2>
 		La ley está vigente a través de la ley de Cristo
-	</h2>
+		</h2>
+
+		<p>
+			Aunque el nuevo testamento abroga la ley de Moisés por completo para los creyentes, por otra parte nos enseña que debemos continuar usándola de una manera particular
+		</p>
+	</section>
 	
-	<h2 class="article_heading">
+	<h2>
 		La ley correctamente interpretada nos ayuda a definir el amor
 	</h2>
 	
-	<h2 class="article_heading">
+	<h2>
 		Examinemos el amor con los 10 mandamientos
 	</h2>
 
