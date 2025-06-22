ec064c48a344262791f7808a75991355e4536664
eduar <eduar@EDUARDO>
2025-06-22 06:37:14 -0400

    upload files

--- auiditing_love_through_the_law.go
+++ auiditing_love_through_the_law.go
@@ -21,21 +21,21 @@ const auiditing_love_through_the_law = `
 			<div class="article_section_idea_justification_items_container">
 				<ol>
 					<li>
-						<span>Rom.7.4-12</span>
 						<span>La Ley, especialmente los diez mandamientos, está abrogada.</span>
+						<span>Rom.7.4-12</span>
 					</li>
 					<li>
+						<span>Estamos bajo la ley de Cristo.</span>
 						<span>1.Cor.9.19-22</span>
 						<span>Gal.6.1-2</span>
-						<span>Estamos bajo la ley de Cristo.</span>
 					</li>
 					<li>
+						<span>La ley de Cristo involucra cumplir todos los mandamientos de la ley interpretándolos a través de las enseñanzas y procedimiento de las epístolas.</span>
 						<span>1.Tim.1.3-11</span>
 						<span>1.Jn.3.1-6</span>
 						<span>1.Tim.5.17-18</span>
 						<span>1.Cor.8.13-15</span>
 						<span>1.Ped.1.13.17</span>
-						<span>La ley de Cristo involucra cumplir todos los mandamientos de la ley interpretándolos a través de las enseñanzas y procedimiento de las epístolas.</span>
 					</li>
 				</ol>			
 			</div>
@@ -55,21 +55,18 @@ const auiditing_love_through_the_law = `
 			<div class="article_section_idea_justification_items_container">
 				<ol>
 					<li>
-						<span>Rom.7.4-12</span>
-						<span>La Ley, especialmente los diez mandamientos, está abrogada.</span>
+						<span>El amor es cumplir los mandamientos.</span>
+						<span>1.Jn.5.1-5</span>
 					</li>
 					<li>
-						<span>1.Cor.9.19-22</span>
-						<span>Gal.6.1-2</span>
-						<span>Estamos bajo la ley de Cristo.</span>
+						<span>El amor espiritual no es la emocionalidad humana, sino el cumplimiento de los mandamientos.</span>
+						<span>1.Jn.5.16-17</span>
 					</li>
 					<li>
-						<span>1.Tim.1.3-11</span>
-						<span>1.Jn.3.1-6</span>
-						<span>1.Tim.5.17-18</span>
-						<span>1.Cor.8.13-15</span>
-						<span>1.Ped.1.13.17</span>
-						<span>La ley de Cristo involucra cumplir todos los mandamientos de la ley interpretándolos a través de las enseñanzas y procedimiento de las epístolas.</span>
+						<span>El amor espiritual no es la emocionalidad humana, sino el cumplimiento de los mandamientos.</span>
+						<span>1.Jn.5.16-17</span>
+						<span>1.Jn.2.4-6</span>
+						<span>Jn.14.21</span>
 					</li>
 				</ol>			
 			</div>
