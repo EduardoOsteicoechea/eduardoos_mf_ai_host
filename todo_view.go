026546a4d4887f1d687b295f6523e497d5b034b3
eduar <eduar@EDUARDO>
2025-06-24 05:31:23 -0400

    upload files

--- todo_view.go
+++ todo_view.go
@@ -43,9 +43,19 @@ func todo_page_view(pageTop string, pageBody string, pageBottom string) string {
 		const todo_app_add_task_input = document.getElementById("todo_app_add_task_input")
 		const todo_app_add_task_button = document.getElementById("todo_app_add_task_button")
 
-		todo_app_add_task_button.addEventListener("click",()=>{
+		todo_app_add_task_button.addEventListener("click",()=>
+		{
+			const todo_app_ordered_list_item_count = todo_app_ordered_list.children.length
+
 			const new_task_container = document.createElement("li")
-			new_task_container.className = "new_task_container"
+			new_task_container.className = "new_task_container"			
+
+			const new_task_item_number = document.createElement("span")
+			new_task_item_number.textContent = todo_app_ordered_list_item_count
+			new_task_item_number.className = "new_task_item_number"
+			
+			const new_task_data_items_container = document.createElement("div")
+			new_task_data_items_container.className = "new_task_data_items_container"
 
 			const new_task_text = document.createElement("p")
 			new_task_text.innerHTML = todo_app_add_task_input.value
@@ -67,12 +77,17 @@ func todo_page_view(pageTop string, pageBody string, pageBottom string) string {
 			})
 			new_task_delete_button.className = "new_task_delete_button"
 
-			new_task_container.appendChild(new_task_text)
+			new_task_data_items_container.appendChild(new_task_text)
 			new_task_date_and_delete_button_container.appendChild(new_task_time)
 			new_task_date_and_delete_button_container.appendChild(new_task_delete_button)
-			new_task_container.appendChild(new_task_date_and_delete_button_container)
+			new_task_data_items_container.appendChild(new_task_date_and_delete_button_container)
+
+			new_task_container.appendChild(new_task_item_number)
+			new_task_container.appendChild(new_task_data_items_container)
 			
 			todo_app_ordered_list.appendChild(new_task_container)
+
+			todo_app_add_task_input.value = ""
 		})
 	</script>
 	`, formatted_date)
