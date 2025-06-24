a67f2b471c9ba7462e522166f00464fa492d90bf
eduar <eduar@EDUARDO>
2025-06-24 06:27:47 -0400

    upload files

--- todo_view.go
+++ todo_view.go
@@ -13,6 +13,9 @@ func todo_page_view(pageTop string, pageBody string, pageBottom string) string {
 	const page_styles string = `
 		<link rel="stylesheet" href="static/todo_app_styles.css">
 	`
+	const page_javascript string = `
+		<script href="static/todo_app_script.js" defer></script>
+	`
 	
 	var todo_view_content string = fmt.Sprintf(`
 	<div class="todo_app_outer_container">
@@ -45,62 +48,9 @@ func todo_page_view(pageTop string, pageBody string, pageBottom string) string {
 
 		</ol>
 	</div>
-	
-	<script id="todo_app_script">
-		const todo_app_ordered_list = document.getElementById("todo_app_ordered_list")
-		const todo_app_add_task_input = document.getElementById("todo_app_add_task_input")
-		const todo_app_add_task_button = document.getElementById("todo_app_add_task_button")
-
-		todo_app_add_task_button.addEventListener("click",()=>
-		{
-			const todo_app_ordered_list_item_count = todo_app_ordered_list.children.length + 1
-
-			const new_task_container = document.createElement("li")
-			new_task_container.className = "new_task_container"			
-
-			const new_task_item_number = document.createElement("span")
-			new_task_item_number.textContent = todo_app_ordered_list_item_count
-			new_task_item_number.className = "new_task_item_number"
-			
-			const new_task_data_items_container = document.createElement("div")
-			new_task_data_items_container.className = "new_task_data_items_container"
-
-			const new_task_text = document.createElement("p")
-			new_task_text.innerHTML = todo_app_add_task_input.value
-			new_task_text.className = "new_task_text"
-			
-			const new_task_date_and_delete_button_container = document.createElement("div")
-			new_task_date_and_delete_button_container.className = "new_task_date_and_delete_button_container"
-			
-			const new_task_time = document.createElement("span")
-			const now = new Date()
-    	const task_time_formatted = now.toLocaleString()
-    	new_task_time.textContent = task_time_formatted
-			new_task_time.className = "new_task_time"
-
-			const new_task_delete_button = document.createElement("button")
-			new_task_delete_button.innerHTML = "X"
-			new_task_delete_button.addEventListener("click",()=>{
-				new_task_container.remove()
-			})
-			new_task_delete_button.className = "new_task_delete_button"
-
-			new_task_data_items_container.appendChild(new_task_text)
-			new_task_date_and_delete_button_container.appendChild(new_task_time)
-			new_task_date_and_delete_button_container.appendChild(new_task_delete_button)
-			new_task_data_items_container.appendChild(new_task_date_and_delete_button_container)
-
-			new_task_container.appendChild(new_task_item_number)
-			new_task_container.appendChild(new_task_data_items_container)
-			
-			todo_app_ordered_list.appendChild(new_task_container)
-
-			todo_app_add_task_input.value = ""
-		})
-	</script>
 	`, formatted_date)
 
-	todo_view := pageTop + page_styles + pageBody + todo_view_content + pageBottom
+	todo_view := pageTop + page_styles + pageBody + todo_view_content + page_javascript + pageBottom
 
 	return todo_view
 }
